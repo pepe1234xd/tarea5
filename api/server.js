c8a6d52cb202e11e7e182b30dfcb81b00edd21ee
pepe1234xd <104106753+pepe1234xd@users.noreply.github.com>
2022-05-16 16:31:56 -0500

    adding a swagger

--- api/server.js
+++ api/server.js
@@ -8,9 +8,16 @@ const port = 5001;
 // JSON Middleware
 app.use(express.json());
 
+const swagger = require(`swagger-ui-express`);
+const swaggerdoc = require(`./swagger.json`);
+
 // Static files
 const folder = path.resolve(__dirname, 'public');
 app.use('/', express.static(folder));
+
+//adding a swagger documentation.
+app.use(`swagger`,swagger.serve);
+app.use(`swagger`,swagger.setup(swaggerdoc));
 
 // Routers
 app.use('/v1', v1);
