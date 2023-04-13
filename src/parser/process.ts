305945f7eeea451d492850c21d1b1f13c0a60a28
Carlos Andres Briones Montellano <nona9614@hotmail.com>
2023-04-13 16:25:57 -0600

    Remove no needed code

--- src/parser/process.ts
+++ src/parser/process.ts
@@ -1,4 +1,4 @@
-import { EmptyValueError, ParseError } from "../validations.js";
+import { EmptyValueError, NotValidUseOfQuotes, ParseError } from "../errors.js";
 import { format } from "../text-format.js";
 import { context } from "./context.js";
 import { CSV } from "../csv/csv.js";
@@ -8,101 +8,45 @@ import { transforms, transformsNumber } from "./transforms.js";
  * Process a word to remove extra quotes to set a clean value
  * @param word The word to be processed
  */
-export function process(csv: CSV) {
-  let word = context.line;
-
-  // If the processed value is a JSON object process it
-  if (context.isJSON) return JSON.parse(context.line);
+export function process() {
+  let word = "";
 
   // Read format options
   const { quote, delimiter, empty } = format;
 
-  // General cases when no characters were found
-  if (word.length === 0) {
-    // If has no content is an empty value
-    return empty;
-  }
-  // General cases when a unique character was found
-  else if (word.length === 1) {
-    // If the value found was a quote symbol, is a parse error
-    if (word === quote) throw ParseError(csv);
-    // If the value found was a delimiter is an empty value
-    else if (word === delimiter) return empty;
-    else return context.shouldTransform ? transformsNumber(word) : word;
-  }
-  // General cases when two characters were found
-  else if (word.length === 2) {
-    // Cases when the word was inside quotes
-    if (context.isQuoted) {
-      // If the character are two quotes return an empty value
-      if (word[0] === quote && word[1] === quote) return empty;
-      // If the characters are a mix of quotes and delimiters is a parse error
-      else if (
-        word[0] === quote ||
-        word[0] === delimiter ||
-        word[1] === quote ||
-        word[1] === delimiter
-      )
-        throw ParseError(csv);
-      // Any other way is a valid word
-      else return context.shouldTransform ? transformsNumber(word) : word;
-    }
-    // Cases when the word was not surrounded by quotes
-    else {
-      // If the first value is a quote it can only be followed by a quote,
-      // and that would mean a "double quote" was found thus
-      // reducing it as a single quote
-      if (word[0] === quote) {
-        if (word[1] === quote) return quote;
-        else throw ParseError(csv);
-      }
-      // If the first value is not a quote it can only be followed by a non quote symbols
-      else {
-        if (word[1] === quote) throw ParseError(csv);
-        else return context.shouldTransform ? transformsNumber(word) : word;
-      }
-    }
-  }
-  // In case of three characters and that it is surrounded by quotes
-  // any non quote symbol inside it is a valid value and will be returned as it is
-  else if (word.length === 3 && context.isQuoted) {
-    if (
-      word[0] === quote &&
-      word[1] !== quote &&
-      word[1] !== delimiter &&
-      word[2] === quote
-    )
-      return context.shouldTransform ? transformsNumber(word[1]) : word[1];
-    else throw ParseError(csv);
-  }
+  // If has no content is an empty value
+  if (context.line.length === 0) return empty;
 
-  // For any other cases as we are in quote mode remove outside quotes
-  if (context.isQuoted) word = word.substring(1, word.length - 1);
+  // If the processed value is a JSON object process it
+  if (context.isJSON) return JSON.parse(context.line);
 
-  // Fort strings longer thatn 3 characters applies below loop logic
-  let _line: string = "";
-  let next: string | undefined;
-  for (let i = 0; i < word.length; i++) {
-    // Saving the next character from the word in the iteration current position
-    // and if there is no next character save an undefined value
-    next = i === word.length - 1 ? undefined : word[i + 1];
-    /** Stores the current character from the iteration */
-    const char = word[i];
-    // If the current char is the quote symbol, it can only be followed by
-    // a quote symbol to be simplified as a single quote symbol
-    if (char === quote) {
-      if (next !== quote) throw ParseError(csv);
-      else {
-        _line = quote;
-        // Will make it skip the following character as it is the next quote
-        i++;
-        continue;
-      }
+  // Resets the regex to be used
+  context.resetQuoteRegex();
+  // The start index to be picking content
+  let startIndex = 0;
+  let array: RegExpExecArray | null;
+  while ((array = context.quoteRegex.exec(context.line)) !== null) {
+    // When a match is present check if the following characters are another quote
+    const isNextQuote: boolean =
+      context.line.substring(
+        context.quoteRegex.lastIndex,
+        context.quoteRegex.lastIndex + quote.length,
+      ) === quote;
+    // If the next value is a quote, simplify the double quotes and save
+    // Then move the last index after the double quotes
+    if (isNextQuote) {
+      word += context.line.substring(startIndex, array.index) + quote;
+      // Move the last index after the double quotes
+      startIndex = context.quoteRegex.lastIndex + quote.length;
+      context.quoteRegex.lastIndex = startIndex;
     }
-    // Any other symbol is a valid symbol and will be stored directly to the line
-    else _line += char;
+    // If the next value is not a quote it is a parse error
+    else throw NotValidUseOfQuotes;
   }
+  // Concatenate any residual content
+  if (startIndex <= context.line.length)
+    word += context.line.substring(startIndex, context.line.length);
 
   // Finally returns the processed line as a parsed string and transform if necessary
-  return context.shouldTransform ? transforms(_line) : _line;
+  return context.shouldTransform ? transforms(word) : word;
 }
--- tests/callee/parser.ts
+++ tests/callee/parser.ts
@@ -4,6 +4,14 @@ import type { TestParser, TestName } from "../types";
 
 import { transforms } from "../../src/parser/transforms";
 import { parse } from "../../src/parser/parse";
+import { process } from "../../src/parser/process";
+import { context, createContext } from "../../src/parser/context";
+import {
+  EMPTY_SYMBOL,
+  setDefaultTextFormat,
+  setTextFormat,
+} from "../../src/text-format";
+import { NotValidUseOfQuotes } from "../../src/errors";
 
 export default function createParserTest(key: TestParser) {
   switch (key) {
@@ -13,6 +21,29 @@ export default function createParserTest(key: TestParser) {
         if (item.expected === "undefined" || item.expected === "UNDEFINED")
           item.expected = undefined;
         expect(_transforms).to.eql(item.expected);
+      });
+    case "process":
+      return _(key, function (item) {
+        createContext(item.word);
+        context.slength = item.word.length;
+        context.line = item.word;
+        context.isQuoted = item.isQuoted;
+        context.isJSON = item.isJSON;
+        if (item.throws === "NotValidUseOfQuotes") {
+          expect(function () {
+            process();
+          }).to.throw(NotValidUseOfQuotes);
+        } else if (typeof item.expected === "string") {
+          const _process = process();
+          if (item.expected === "undefined" || item.expected === "UNDEFINED")
+            item.expected = undefined;
+          else if (item.expected === 'Symbol("empty")')
+            item.expected = EMPTY_SYMBOL;
+          expect(_process).to.equals(item.expected);
+        } else {
+          const _process = process();
+          expect(_process).to.eql(item.expected);
+        }
       });
     case "parse":
       return _(key, function (item) {
